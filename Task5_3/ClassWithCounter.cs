6e6a0c51519e257133b7e499e01742b8a6d592bf
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-09-27 13:13:18 +0300

    Add project files.

---
+++ Task5_3/ClassWithCounter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_3
+{
+    class ClassWithCounter
+    {
+        private static int counter = 0;
+        public static int CheckCounter()
+        {
+            return counter;
+        }
+        public ClassWithCounter()
+        {
+            counter++;
+        }
+    }
+}
---
+++ Task5_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassWithCounter c1 = new ClassWithCounter();
+            ClassWithCounter c2 = new ClassWithCounter();
+            ClassWithCounter c3 = new ClassWithCounter();
+            ClassWithCounter c4 = c3;
+            Console.WriteLine(ClassWithCounter.CheckCounter());
+            Console.ReadKey();
+        }
+    }
+}
